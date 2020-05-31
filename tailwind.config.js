cd06cbd3a2628654f445e5b2eeaa5f485de70757
Lynn Baxter <lynnmariebee@gmail.com>
2020-05-31 07:45:26 -0700

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      // 'display': ['Oswald'],
+      'sans': ['Source Sans Pro']
+    },
+    extend: {},
+    textColor: {
+      'bamboo': '#DF5F02',
+      'outerspace': '#272B2A',
+      'nandor': '#505857',
+      'silversand' : '#BFC5C4',
+      'iron' :'#DADDDC',
+      'tpblue' : '#4272CF',
+      'ppgreen' : '#6A9086',
+      'nborange' : '#FB6F08',
+      },
+    borderColor: {
+      'bamboo': '#DF5F02',
+      }
+  },
+  variants: {},
+  plugins: [],
+}
