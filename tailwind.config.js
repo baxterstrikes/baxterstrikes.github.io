4fd8fa20c5cd5ad96f8c376092e38900e5822616
Lynn Baxter <lynnmariebee@gmail.com>
2020-07-18 13:54:08 -0700

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,29 @@ module.exports = {
       // 'display': ['Oswald'],
       'sans': ['Source Sans Pro']
     },
+    fontSize: {
+      "H1": "48px",
+      "H2": "36px",
+      "H3": "32px",
+      "B1": "28px",
+      "B2": "24px",
+      "B3": "20px",
+      "B4": "18px"
+    },
     extend: {},
     textColor: {
       'bamboo': '#DF5F02',
       'outerspace': '#272B2A',
       'nandor': '#505857',
-      'silversand' : '#BFC5C4',
-      'iron' :'#DADDDC',
-      'tpblue' : '#4272CF',
-      'ppgreen' : '#6A9086',
-      'nborange' : '#FB6F08',
-      },
+      'silversand': '#BFC5C4',
+      'iron': '#DADDDC',
+      'tpblue': '#4272CF',
+      'ppgreen': '#6A9086',
+      'nborange': '#FB6F08',
+    },
     borderColor: {
       'bamboo': '#DF5F02',
-      }
+    }
   },
   variants: {},
   plugins: [],
